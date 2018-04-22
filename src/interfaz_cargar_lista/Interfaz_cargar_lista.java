3113235ae6ec340571a72344ea40b4cb97f728a7
leonardodavid <leonardodavidcba@gmail.com>
2018-04-22 17:46:56 -0300

    algo

---
+++ src/interfaz_cargar_lista/Interfaz_cargar_lista.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaz_cargar_lista;
+
+import presentacion.*;
+
+/**
+ *
+ * @author leonardodavod
+ */
+public class Interfaz_cargar_lista 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       
+        Ventana iniciar = new Ventana();
+        iniciar.setVisible(true);
+        
+    }
+    
+}
---
+++ src/modelo/Continente.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author leonardodavod
+ */
+public class Continente 
+{
+    private String nombre;
+    private String observacion;
+    private int codigo;
+
+    
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Continente{" + "nombre=" + nombre + ", observacion=" + observacion + ", codigo=" + codigo + '}';
+    }
+    
+    
+    
+    
+}
